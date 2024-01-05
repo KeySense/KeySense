7519d4cddffb74fde44dfabda4d00a9e068a6ed9
Liam <iliamftw2013@gmail.com>
2024-01-05 18:43:34 -0300

    Add(Init Files)

---
+++ KeySense/Client/Client.py
@@ -0,0 +1,8 @@
+# Authors: Liam Arguedas <iliamftw2013@gmail.com>
+# License: BSD 3 clause
+
+
+class Client:
+    def __init__(self):
+        self.path = script_path
+        self.language = client_language
